743831b6fac7791fd74aeddfc4444dfd0753732a
Conrad Koh <conradkoh@gmail.com>
2020-04-25 01:27:03 +0800

    Added support for encrypting files

--- src/test/lib/encryption.js
+++ src/test/lib/encryption.js
@@ -3,13 +3,29 @@ import '../../helpers/test/setup';
 import { createEncryptor } from '../../lib/encryption';
 import crypto from 'crypto';
 import { getConfig } from '../../config/ConfigurationManager';
+import fs from 'fs';
+import path from 'path';
 describe('Encryption', () => {
     it('Encodes and decodes data correctly', async () => {
         const encryptionKey = getConfig().encyption_key;
-        let data = 'abc123';
+        let data = Buffer.from('abc123');
         const encrpytor = createEncryptor(encryptionKey);
         const { encryptedData, iv } = await encrpytor.encode(data);
-        const val = await encrpytor.decode(encryptedData, iv);
-        assert.equal(val, data, 'Encryption / Decryption failed.');
+        const decryptedData = await encrpytor.decode(encryptedData, iv);
+        assert.isTrue(Buffer.compare(data, decryptedData) === 0, 'Encryption / Decryption failed.');
+    });
+    it('Encodes and decodes files correctly', async () => {
+        const TEST_FILE_PATH = path.join(__dirname, '../data/test.png');
+        /** @type {Buffer} */
+        const data = await new Promise((resolve, reject) => {
+            fs.readFile(TEST_FILE_PATH, (err, data) => {
+                err ? reject(err) : resolve(data);
+            });
+        });
+        const encryptionKey = getConfig().encyption_key;
+        const encrpytor = createEncryptor(encryptionKey);
+        const { encryptedData, iv } = await encrpytor.encode(data);
+        const decryptedData = await encrpytor.decode(encryptedData, iv);
+        assert.isTrue(Buffer.compare(data, decryptedData) === 0, 'Encryption / Decryption failed.');
     });
 });
