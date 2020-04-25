34b083bdb9c48932c8c07ad0b4f60bacfac95920
Conrad Koh <conradkoh@gmail.com>
2020-04-25 11:11:03 +0800

    Linked Profile API to route, added documentation for date of birth in user profile

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import profileRouter from './profile';
 const router = express.Router();
 router.use('/', express.static(path.join(__dirname, '../../doc')));
+router.use('/profile', profileRouter);
 export default router;
--- src/api/profile.js
+++ src/api/profile.js
@@ -9,12 +9,14 @@ const router = express.Router();
  *
  *
  * @apiParam  {String} fullName Full name of the user
+ * @apiParam  {String} dateOfBirth The date of birth of the user in YYYY-MM-DD format
  *
  * @apiSuccess (200) {String} status Status of the request
  *
  * @apiParamExample  {json} Request-Example:
  * {
- *     "fullName" : "John Doe"
+ *     "fullName" : "John Doe",
+ *     "dateOfBirth": "1983-10-11"
  * }
  *
  *
